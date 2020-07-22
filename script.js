5005217a49f72c7e69007a83ba994c996ab94a2b
piaspro <golamrahman09@gmail.com>
2020-07-22 14:37:46 +0600

    withdraw added

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ login.addEventListener('click', function () {
 })
 // deposit event handler
 const depositBtn = document.getElementById('deposit');
-depositBtn.addEventListener('click ', function () {
+depositBtn.addEventListener('click', function () {
     const depositAmount = document.getElementById('depositInput').value;
     const depositNumber = parseFloat(depositAmount);
 
@@ -17,4 +17,30 @@ depositBtn.addEventListener('click ', function () {
 
     document.getElementById('depositAdded').innerText = totalDeposit;
     document.getElementById('depositInput').value = "";
+
+    const balance = document.getElementById('totalBalance').innerText;
+    const floatedBalance = parseFloat(balance);
+    const finalBalance = totalDeposit + floatedBalance;
+
+    document.getElementById('totalBalance').innerText = finalBalance;
+})
+
+// withdraw event handler
+const withdrawBtn = document.getElementById('withdraw-btn');
+withdrawBtn.addEventListener('click', function () {
+    const withdrawAmount = document.getElementById('withdraw').value;
+    const withdrawNumber = parseFloat(withdrawAmount);
+
+    const withdrawSubtract = document.getElementById('withdrawMoney').innerText;
+    const currentWithdraw = parseFloat(withdrawSubtract);
+    const totalWithdraw = currentWithdraw + withdrawNumber;
+
+    document.getElementById('withdrawMoney').innerText = totalWithdraw;
+    document.getElementById('withdraw').value = "";
+
+    const balance = document.getElementById('totalBalance').innerText;
+    const floatedBalance = parseFloat(balance);
+    const finalBalance = floatedBalance - totalWithdraw;
+
+    document.getElementById('totalBalance').innerText = finalBalance;
 })
